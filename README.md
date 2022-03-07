367650da443f572312eea07cffe200373cf83455
SpookySuede <spookysuede@gmail.com>
2022-03-07 17:50:57 -0500

    Final edits.
    Turned 4 Strings into one for sake of cleanliness.
    
    Added userInput Scanner to handle response event.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Dragon Cave Project
  Week 1 project for Genspark Java Cohort
+
+Titled strangely due to a pursuit of "creative freedom/expression" but lost momentum and decided to stick with the script...
--- src/main/java/StarSearch.java
+++ src/main/java/StarSearch.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class StarSearch {
     public static void main(String[] args){
-        String line1 = "You are in a land full of dragons. In front of you,";
-        String line2 = "you see two caves. In one cave, the dragon is friendly";
-        String line3 = "and will share his treasure with you. The other dragon";
-        String line4 = "is greedy and hungry and will eat you on site.";
 
+        String statement =  "You are in a land full of dragons. In front of you,\n \n" +
+                "you see two caves. In one cave, the dragon is friendly\n \n" +
+                "and will share his treasure with you. The other dragon\n \n" +
+                "is greedy and hungry and will eat you on site.\n \n";
+                //this could have been concatenated however i didn't  quite see the need to do so
+                //and this looks a lot cleaner/is easier to read.
 
-        System.out.print(question);
+        String response = "\n\nYou approach the cave...\n\n" +
+                "It is dark and spooky...\n\n" +
+                "A large dragon jumps out in front of you! He opens his jaws and...\n\n" +
+                "Gobbles you down in one bite!\n\n";
+
+        System.out.print(statement);
+        System.out.print("Which cave will you go into? (1 or 2)" + "\n\n");
+
+        Scanner userInput = new Scanner(System.in);
+
+        String input = userInput.nextLine();
+        System.out.print(response);
+
     };
 
 }
