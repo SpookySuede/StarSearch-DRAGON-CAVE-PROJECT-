e4e8b6cfed56af9c571d4cb2cdb013e91cbd6259
SpookySuede <spookysuede@gmail.com>
2022-03-07 17:16:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dragon Cave Project
+ Week 1 project for Genspark Java Cohort
---
+++ src/main/java/StarSearch.java
@@ -0,0 +1,12 @@
+public class StarSearch {
+    public static void main(String[] args){
+        String line1 = "You are in a land full of dragons. In front of you,";
+        String line2 = "you see two caves. In one cave, the dragon is friendly";
+        String line3 = "and will share his treasure with you. The other dragon";
+        String line4 = "is greedy and hungry and will eat you on site.";
+
+
+        System.out.print(question);
+    };
+
+}
